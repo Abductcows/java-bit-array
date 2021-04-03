53fac3cc33ced12b543c14fc162d1cd053222db9
George Bouroutzoglou <geompokon@csd.auth.gr>
2021-04-03 20:54:15 +0300

    what are default implementations?

--- src/gr/geompokon/bitarray/BitArray.java
+++ src/gr/geompokon/bitarray/BitArray.java
@@ -1,32 +1,19 @@
 package gr.geompokon.bitarray;
 
-import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.IntFunction;
-import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
-import java.util.stream.Stream;
+import java.util.AbstractList;
+import java.util.RandomAccess;
 
 public class BitArray extends AbstractList<Boolean> implements RandomAccess {
 
-    BitArrayImpl array;
-    private ArrayList<Boolean> reference;
+    private final static int DEFAULT_CAPACITY = 10;
+    private BitArrayImpl array;
 
     BitArray() {
-        array = new BitArrayImpl();
+        this(DEFAULT_CAPACITY);
     }
+
     BitArray(int initialSize) {
         array = new BitArrayImpl(initialSize);
-    }
-
-    @Override
-    public boolean add(Boolean bit) {
-        return array.add(bit);
-    }
-
-    @Override
-    public Boolean set(int index, Boolean bit) {
-        return array.set(index, bit);
     }
 
     @Override
@@ -35,178 +22,18 @@ public class BitArray extends AbstractList<Boolean> implements RandomAccess {
     }
 
     @Override
-    public Boolean remove(int index) {
-        return array.remove(index);
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        Iterator<Boolean> it = this.iterator();
-        int index = 0;
-        while (it.hasNext()) {
-            if (it.next().equals(o)) {
-                return index;
-            }
-            ++index;
-        }
-        return -1;
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        Iterator<Boolean> it = this.iterator();
-        int index = 0, lastIndex = -1;
-        while (it.hasNext()) {
-            if (it.next().equals(o)) {
-                lastIndex = index;
-            }
-            ++index;
-        }
-        return lastIndex;
-    }
-
-    @Override
-    public void clear() {
-        // TODO: add clear method to BitArrayImpl
-        super.clear();
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends Boolean> c) {
-        // TODO
-        return super.addAll(index, c);
-    }
-
-    @Override
-    public Iterator<Boolean> iterator() {
-        return new BitListIterator();
-    }
-
-    @Override
-    public ListIterator<Boolean> listIterator() {
-        // TODO
-        return super.listIterator();
-    }
-
-    @Override
-    public ListIterator<Boolean> listIterator(int index) {
-        // TODO
-        return super.listIterator(index);
-    }
-
-    @Override
-    public List<Boolean> subList(int fromIndex, int toIndex) {
-        // TODO
-        return super.subList(fromIndex, toIndex);
-    }
-
-    @Override
-    protected void removeRange(int fromIndex, int toIndex) {
-        // TODO
-        super.removeRange(fromIndex, toIndex);
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return array.isEmpty();
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return indexOf(o) >= 0;
-    }
-
-    @Override
-    public Object[] toArray() {
-        // TODO
-        return super.toArray();
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        // TODO
-        return super.toArray(a);
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        // TODO
-        return super.remove(o);
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        // TODO
-        return super.containsAll(c);
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends Boolean> c) {
-        // TODO
-        return super.addAll(c);
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        // TODO
-        return super.removeAll(c);
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        // TODO
-        return super.retainAll(c);
-    }
-
-    @Override
-    public void replaceAll(UnaryOperator<Boolean> operator) {
-        // TODO: implement with ListIterator?
-    }
-
-    @Override
-    public void sort(Comparator<? super Boolean> c) {
-        // TODO
-    }
-
-    @Override
-    public Spliterator<Boolean> spliterator() {
-        // TODO
-        return null;
-    }
-
-    @Override
-    public <T> T[] toArray(IntFunction<T[]> generator) {
-        // TODO
-        return null;
-    }
-
-    @Override
-    public boolean removeIf(Predicate<? super Boolean> filter) {
-        // TODO
-        return false;
-    }
-
-    @Override
-    public Stream<Boolean> stream() {
-        // TODO
-        return null;
-    }
-
-    @Override
-    public Stream<Boolean> parallelStream() {
-        // TODO
-        return null;
-    }
-
-    @Override
-    public void forEach(Consumer<? super Boolean> action) {
-        Iterator<Boolean> it = this.iterator();
-        it.forEachRemaining(action);
-    }
-
-    @Override
     public Boolean get(int index) {
         return array.get(index);
+    }
+
+    @Override
+    public Boolean set(int index, Boolean bit) {
+        return array.set(index, bit);
+    }
+
+    @Override
+    public Boolean remove(int index) {
+        return array.remove(index);
     }
 
     @Override
@@ -214,81 +41,9 @@ public class BitArray extends AbstractList<Boolean> implements RandomAccess {
         return array.size();
     }
 
-    private class BitListIterator implements ListIterator<Boolean> {
-
-        int currentIndex = -1;
-        boolean previousCalled = false, nextCalled = false;
-
-        @Override
-        public boolean hasNext() {
-            return currentIndex + 1 < array.size();
-        }
-
-        @Override
-        public Boolean next() {
-            if (hasNext()) {
-                nextCalled = true;
-                previousCalled = false;
-                return array.get(++currentIndex);
-            } else {
-                throw new NoSuchElementException();
-            }
-        }
-
-        @Override
-        public boolean hasPrevious() {
-            return currentIndex >= 0;
-        }
-
-        @Override
-        public Boolean previous() {
-            if (hasPrevious()) {
-                previousCalled = true;
-                nextCalled = false;
-                return array.get(currentIndex--);
-            } else {
-                throw new NoSuchElementException();
-            }
-        }
-
-        @Override
-        public int nextIndex() {
-            return currentIndex + 1;
-        }
-
-        @Override
-        public int previousIndex() {
-            return currentIndex;
-        }
-
-        @Override
-        public void remove() {
-            if (nextCalled) {
-                array.remove(currentIndex + 1);
-                nextCalled = false;
-            } else if (previousCalled) {
-                array.remove(currentIndex);
-                currentIndex = currentIndex - 1;
-            } else {
-                throw new IllegalStateException("remove called with no element selected");
-            }
-        }
-
-        @Override
-        public void set(Boolean aBoolean) {
-            if (nextCalled) {
-                array.set(currentIndex + 1, aBoolean);
-                nextCalled = false;
-            } else if (previousCalled) {
-                array.set(currentIndex, aBoolean);
-            } else {
-                throw new IllegalStateException("remove called with no element selected");
-            }
-        }
-
-        @Override
-        public void add(Boolean aBoolean) {
-            array.add(currentIndex + 1, aBoolean);
-        }
+    @Override
+    public void clear() {
+        // TODO: implement clear in BitArrayImpl
+        super.clear();
     }
 }
--- test/gr/geompokon/bitarray/BitArrayTest.java
+++ test/gr/geompokon/bitarray/BitArrayTest.java
@@ -3,33 +3,37 @@ package gr.geompokon.bitarray;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
-import java.util.ListIterator;
-import java.util.NoSuchElementException;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BitArrayTest {
 
-    final static int TEST_SIZE = 2000;
+    final static int MAX_TEST_SIZE = 500;
 
-    static BitArray bitArray;
-    static ArrayList<Boolean> boolArray;
+    static AbstractList<Boolean> bitArray;
+    static AbstractList<Boolean> boolArray;
     Random rand;
 
     @BeforeEach
     void setUp() {
-        bitArray = new BitArray(TEST_SIZE);
-        boolArray = new ArrayList<>(TEST_SIZE);
+        bitArray = new BitArray(MAX_TEST_SIZE);
+        boolArray = new ArrayList<>(MAX_TEST_SIZE);
         rand = new Random();
     }
 
+    /**
+     * Clears both lists and adds the same {@code noOfElements} random booleans to both. That is, a call to
+     * boolArray.equals(bitArray) is always successful afterwards
+     *
+     * @param noOfElements number of random booleans to be added
+     */
     void initArrays(int noOfElements) {
-        bitArray = new BitArray(noOfElements);
-        boolArray = new ArrayList<>(noOfElements);
+        bitArray.clear();
+        boolArray.clear();
         for (int i = 0; i < noOfElements; i++) {
             boolean element = rand.nextBoolean();
             bitArray.add(element);
@@ -37,39 +41,29 @@ class BitArrayTest {
         }
     }
 
+    /**
+     * Asserts that the two lists have the same exact contents
+     */
     void myAssertSameArrays() {
-        assertEquals(bitArray.size(), boolArray.size());
-        for (int i = 0; i < bitArray.size(); i++) {
-            assertEquals(bitArray.get(i), boolArray.get(i));
-        }
-    }
-
-
-    @Test
-    void add() {
-        initArrays(TEST_SIZE);
-        myAssertSameArrays();
+        assertEquals(boolArray, bitArray);
     }
 
     @Test
-    void addIndex() {
-        initArrays(10);
-
-        for (int i = 0; i < TEST_SIZE; i++) {
-            int index = rand.nextInt(bitArray.size());
-            Boolean negatedElement = !bitArray.get(index);
-            bitArray.add(index, negatedElement);
-            boolArray.add(index, negatedElement);
+    void addAtIndex() {
+        for (int i = 0; i < MAX_TEST_SIZE; i++) {
+            int index = rand.nextInt(bitArray.size() + 1);
+            boolean element = rand.nextBoolean();
+            bitArray.add(index, element);
+            boolArray.add(index, element);
         }
-
         myAssertSameArrays();
     }
 
     @Test
     void set() {
-        initArrays(TEST_SIZE);
+        initArrays(MAX_TEST_SIZE);
 
-        for (int i = 0; i < TEST_SIZE; i++) {
+        for (int i = 0; i < MAX_TEST_SIZE; i++) {
             int index = rand.nextInt(bitArray.size());
             Boolean negatedElement = !bitArray.get(index);
             bitArray.set(index, negatedElement);
@@ -81,14 +75,13 @@ class BitArrayTest {
 
     @Test
     void remove() {
-        initArrays(TEST_SIZE);
+        initArrays(MAX_TEST_SIZE);
 
-        for (int i = 0; i < TEST_SIZE; i++) {
+        for (int i = 0; i < MAX_TEST_SIZE; i++) {
             int index = rand.nextInt(bitArray.size());
             bitArray.remove(index);
             boolArray.remove(index);
         }
-
         myAssertSameArrays();
 
         while (!bitArray.isEmpty()) {
@@ -99,300 +92,21 @@ class BitArrayTest {
     }
 
     @Test
-    void indexOf() {
-        for (int i = 0; i < 10; i++) {
-            initArrays(10);
-            assertEquals(boolArray.indexOf(Boolean.FALSE), bitArray.indexOf(Boolean.FALSE));
-            assertEquals(boolArray.indexOf(Boolean.TRUE), bitArray.indexOf(Boolean.TRUE));
-        }
-        assertEquals(-1, bitArray.indexOf("not here"));
-    }
-
-    @Test
-    void lastIndexOf() {
-        for (int i = 0; i < 10; i++) {
-            initArrays(10);
-            assertEquals(boolArray.lastIndexOf(Boolean.FALSE), bitArray.lastIndexOf(Boolean.FALSE));
-            assertEquals(boolArray.lastIndexOf(Boolean.TRUE), bitArray.lastIndexOf(Boolean.TRUE));
-        }
-        assertEquals(-1, bitArray.indexOf("not here"));
-    }
-
-    @Test
     void clear() {
-    }
-
-    @Test
-    void addAll() {
-    }
-
-    @Test
-    void iterator() {
-    }
-
-    @Test
-    void listIteratorNextPrev() {
-        bitArray.add(0, true);
-        bitArray.add(1, true);
-        bitArray.add(2, false);
-        ListIterator<Boolean> it = bitArray.listIterator();
-
-        // previous fail
-        assertFalse(it.hasPrevious());
-        assertThrows(NoSuchElementException.class, it::previous);
-
-        // next success
-        assertTrue(it.hasNext());
-        assertDoesNotThrow(it::next);
-
-        // move to the end of the list
-        assertTrue(it.hasNext());
-        assertDoesNotThrow(it::next);
-        assertTrue(it.hasNext());
-        assertDoesNotThrow(it::next);
-
-        // next fail
-        assertFalse(it.hasNext());
-        assertThrows(NoSuchElementException.class, it::next);
-
-        // previous success
-        assertTrue(it.hasPrevious());
-        assertDoesNotThrow(it::previous);
-    }
-
-    @Test
-    void listIteratorNextPrevIndices() {
-        Runnable test = () -> {
-            ListIterator<Boolean> it = bitArray.listIterator();
-            int i = 0;
-            while (it.hasNext()) {
-                assertEquals(i, it.nextIndex());
-                assertEquals(i - 1, it.previousIndex());
-                i = i + 1;
-                it.next();
-            }
-        };
-
-        // test with empty array
-        test.run();
-
-        // test with elements
-        initArrays(30);
-        test.run();
-    }
-
-    @Test
-    void listIteratorRemove() {
         initArrays(10);
-        ListIterator<Boolean> bitIt = bitArray.listIterator();
-
-        // plain remove should fail
-        assertThrows(IllegalStateException.class, bitIt::remove);
-        // remove next
-        bitIt.next();
-        assertDoesNotThrow(bitIt::remove);
-        assertThrows(IllegalStateException.class, bitIt::remove);
-        // move forward and remove previous
-        bitIt.next();
-        bitIt.previous();
-        assertDoesNotThrow(bitIt::remove);
-        assertThrows(IllegalStateException.class, bitIt::remove);
-
-        // remove all elements with next
-        initArrays(100);
-        bitIt = bitArray.listIterator();
-        // also check if it is the same as the ArrayList for every remove
-        ListIterator<Boolean> boolIt = boolArray.listIterator();
-        while (bitIt.hasNext()) {
-            bitIt.next();
-            boolIt.next();
-            bitIt.remove();
-            boolIt.remove();
-            myAssertSameArrays();
-        }
-        assertEquals(0, bitArray.size());
-
-        // remove all elements with previous
-        initArrays(100);
-        bitIt = bitArray.listIterator();
-        boolIt = boolArray.listIterator();
-        while (bitIt.hasNext()) {
-            bitIt.next();
-            boolIt.next();
-        }
-        while (bitIt.hasPrevious()) {
-            bitIt.previous();
-            boolIt.previous();
-            bitIt.remove();
-            boolIt.remove();
-            myAssertSameArrays();
-        }
-        assertEquals(0, bitArray.size());
-    }
-
-    @Test
-    void listIteratorSet() {
-        initArrays(30);
-        ListIterator<Boolean> bitIt = bitArray.listIterator();
-        ListIterator<Boolean> boolIt = boolArray.listIterator();
-
-        // set with next
-        boolean value = !bitIt.next();
-        boolIt.next();
-        bitIt.set(value);
-        boolIt.set(value);
-        myAssertSameArrays();
-
-        // set with previous
-        bitIt.next();
-        boolIt.next();
-        value = !bitIt.previous();
-        boolIt.previous();
-        bitIt.set(value);
-        boolIt.set(value);
-        myAssertSameArrays();
-
-        // negate entire array with set
-        initArrays(100);
-
-        bitIt = bitArray.listIterator();
-        while (bitIt.hasNext()) {
-            boolean negatedValue = !bitIt.next();
-            bitIt.set(negatedValue);
-        }
-
-        // negate bool array
-        boolArray = boolArray.stream().map(b -> !b).collect(Collectors.toCollection(ArrayList::new));
-        myAssertSameArrays();
-    }
-
-    @Test
-    void listIteratorAdd() {
-        initArrays(100);
-        // clear bitArray and add every element to the bitArray using iterator
-        bitArray = new BitArray();
-        ListIterator<Boolean> it = bitArray.listIterator();
-
-        for (Boolean aBoolean : boolArray) {
-            it.add(aBoolean);
-        }
-        myAssertSameArrays();
-
-        // test if previous returns the newly added element
-        initArrays(0);
-        assertEquals(0, bitArray.size());
-        it = bitArray.listIterator();
-
-        for (int i = 0; i < 100; i++) {
-            Boolean element = rand.nextBoolean();
-            it.add(element);
-            assertEquals(element, it.previous());
-        }
-
-        // test that add does not move the cursor
-        initArrays(10);
-        it = bitArray.listIterator();
-
-        for (int i = 0; i < 100; i++) {
-            Boolean nextElement = it.next();
-            it.previous();
-            Boolean toAdd = !nextElement; // to make sure it is different
-            it.add(toAdd);
-            assertEquals(nextElement, it.next());
-            it.previous();
-        }
-    }
-
-    @Test
-    void subList() {
-    }
-
-    @Test
-    void testEquals() {
-    }
-
-    @Test
-    void testHashCode() {
-    }
-
-    @Test
-    void removeRange() {
-    }
-
-    @Test
-    void isEmpty() {
-    }
-
-    @Test
-    void contains() {
-    }
-
-    @Test
-    void toArray() {
-    }
-
-    @Test
-    void testToArray() {
-    }
-
-    @Test
-    void testRemove() {
-    }
-
-    @Test
-    void containsAll() {
-    }
-
-    @Test
-    void testAddAll() {
-    }
-
-    @Test
-    void removeAll() {
-    }
-
-    @Test
-    void retainAll() {
-    }
-
-    @Test
-    void replaceAll() {
-    }
-
-    @Test
-    void sort() {
-    }
-
-    @Test
-    void spliterator() {
-    }
-
-    @Test
-    void testToArray1() {
-    }
-
-    @Test
-    void removeIf() {
-    }
-
-    @Test
-    void stream() {
-    }
-
-    @Test
-    void parallelStream() {
+        bitArray.clear();
+        assertTrue(bitArray.isEmpty());
     }
 
     @Test
     void size() {
-        initArrays(TEST_SIZE);
-        int expectedSize = TEST_SIZE;
+        initArrays(MAX_TEST_SIZE);
+        int expectedSize = MAX_TEST_SIZE;
         assertEquals(expectedSize, bitArray.size());
 
-        int noToRemove = 10;
-        for (int i = 0; i < noToRemove; i++) {
-            bitArray.remove(0);
-        }
+        int noToRemove = MAX_TEST_SIZE / 2;
+        bitArray.subList(0, noToRemove).clear();
+
         expectedSize -= noToRemove;
         assertEquals(expectedSize, bitArray.size());
 
@@ -400,24 +114,5 @@ class BitArrayTest {
         for (int i = 0; i < noToAdd; i++) {
             bitArray.add(rand.nextBoolean());
         }
-    }
-
-    @Test
-    void forEach() {
-        initArrays(TEST_SIZE);
-
-        int i = 0;
-        for (boolean bitElement : bitArray) {
-            boolean boolElement = boolArray.get(i);
-            assertEquals(boolElement, bitElement);
-            ++i;
-        }
-
-        AtomicReference<Integer> j = new AtomicReference<>(0);
-        bitArray.forEach(bitElement -> {
-            boolean boolElement = boolArray.get(j.get());
-            assertEquals(bitElement, boolElement);
-            j.set(j.get() + 1);
-        });
     }
 }
