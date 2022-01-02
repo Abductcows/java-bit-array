4299d4b4108233ce0cd4f9cf147aec1133b12de1
geompokon@csd.auth.gr <geompokon@csd.auth.gr>
2022-01-02 21:03:23 +0200

    migrate TestUtils to kotlin
    hell yeah

--- src/test/kotlin/io/github/abductcows/bitarray/TestUtils.kt
+++ src/test/kotlin/io/github/abductcows/bitarray/TestUtils.kt
@@ -1,98 +1,79 @@
-package io.github.abductcows.bitarray;
+package io.github.abductcows.bitarray
 
-import org.junit.jupiter.api.Named;
+import org.junit.jupiter.api.Named
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+object TestUtils {
+    private const val SEED = 69L
+    private const val BITS_PER_LONG = 64
+    private val consistentRandom = Random(SEED)
+    private val booleanListsElementCounts = listOf(
+        0,
+        1,
+        2,
+        3,
+        5,
+        BITS_PER_LONG - 1,
+        BITS_PER_LONG,
+        BITS_PER_LONG + 1,
+        2 * BITS_PER_LONG - 1,
+        2 * BITS_PER_LONG,
+        2 * BITS_PER_LONG + 1
+    )
 
-public class TestUtils {
-
-    static final int SEED = 69;
-    static final int BITS_PER_LONG = 64;
-    static final Random consistentRandom = new Random(SEED);
-
-    static Stream<Named<List<Boolean>>> testCaseBooleans() {
-        synchronized (consistentRandom) {
-            consistentRandom.setSeed(SEED);
-            List<Integer> elementCounts = List.of(
-                    0,
-                    1,
-                    2,
-                    BITS_PER_LONG - 1,
-                    BITS_PER_LONG,
-                    BITS_PER_LONG + 1,
-                    2 * BITS_PER_LONG + 1);
-            Stream.Builder<Named<List<Boolean>>> builder = Stream.builder();
-
-            for (int elementCount : elementCounts) {
-                builder.add(Named.of(
-                        Integer.toString(elementCount),
-                        Stream.generate(consistentRandom::nextBoolean).limit(elementCount).collect(Collectors.toList()))
-                );
+    /**
+     * Lists of boolean elements returned by [consistentRandom]
+     */
+    @JvmStatic
+    fun testCaseBooleans(): Iterable<Named<List<Boolean>>> = synchronizedWithFreshRandom {
+        sequence {
+            for (elementCount in booleanListsElementCounts) {
+                yield(Named.of(
+                    elementCount.toString(),
+                    List(elementCount) { consistentRandom.nextBoolean() }
+                ))
             }
-
-            return builder.build();
-        }
+        }.asIterable()
     }
 
-    static Stream<Named<List<Boolean>>> allSameBooleans() {
-        List<Integer> elementCounts = List.of(
-                1,
-                2,
-                BITS_PER_LONG - 1,
-                BITS_PER_LONG,
-                BITS_PER_LONG + 1,
-                2 * BITS_PER_LONG + 1);
-        Stream.Builder<Named<List<Boolean>>> builder = Stream.builder();
-        for (int elementCount : elementCounts) {
-            builder.add(Named.of(
-                    String.format("%d false", elementCount),
-                    Collections.nCopies(elementCount, Boolean.FALSE))
-            );
-            builder.add(Named.of(
-                    String.format("%d true", elementCount),
-                    Collections.nCopies(elementCount, Boolean.TRUE)
-            ));
+    /**
+     * Lists with all true and all false
+     */
+    @JvmStatic
+    fun allSameBooleans(): Iterable<Named<List<Boolean>>> = sequence {
+        for (elementCount in booleanListsElementCounts.filter { it > 0 }) {
+            yield(Named.of(
+                "%d false".format(elementCount),
+                Collections.nCopies(elementCount, false)))
+            yield(Named.of(
+                "%d true".format(elementCount),
+                Collections.nCopies(elementCount, true)
+            ))
         }
+    }.asIterable()
 
-        return builder.build();
+    @JvmStatic
+    fun getAddIndices(noOfElements: Int): List<Int> = synchronizedWithFreshRandom {
+        consistentRandom.setSeed(SEED)
+        val nextMax = AtomicInteger(1)
+        List(noOfElements) { consistentRandom.nextInt(nextMax.getAndIncrement()) }
     }
 
-    static List<Integer> getAddIndices(int noOfElements) {
-        synchronized (consistentRandom) {
-            consistentRandom.setSeed(SEED);
-            AtomicInteger nextMax = new AtomicInteger(1);
-
-            return Stream
-                    .generate(() -> consistentRandom.nextInt(nextMax.getAndIncrement()))
-                    .limit(noOfElements)
-                    .collect(Collectors.toList());
-        }
+    @JvmStatic
+    fun getSetIndices(noOfElements: Int): List<Int> = synchronizedWithFreshRandom {
+        List(noOfElements) { consistentRandom.nextInt(noOfElements) }
     }
 
-    static List<Integer> getSetIndices(int noOfElements) {
-        synchronized (consistentRandom) {
-            consistentRandom.setSeed(SEED);
-
-            return Stream.generate(() -> consistentRandom.nextInt(noOfElements))
-                    .limit(noOfElements)
-                    .collect(Collectors.toList());
-        }
+    @JvmStatic
+    fun getRemoveIndices(noOfElements: Int): List<Int> = synchronizedWithFreshRandom {
+        val nextMax = AtomicInteger(noOfElements)
+        List(noOfElements) { consistentRandom.nextInt(nextMax.getAndDecrement()) }
     }
 
-    static List<Integer> getRemoveIndices(int noOfElements) {
-        synchronized (consistentRandom) {
-            consistentRandom.setSeed(SEED);
-            AtomicInteger nextMax = new AtomicInteger(noOfElements);
-
-            return Stream
-                    .generate(() -> consistentRandom.nextInt(nextMax.getAndDecrement()))
-                    .limit(noOfElements)
-                    .collect(Collectors.toList());
-        }
+    @JvmStatic
+    fun <T> synchronizedWithFreshRandom(block: () -> T): T = synchronized(consistentRandom) {
+        consistentRandom.setSeed(SEED)
+        block()
     }
 }
